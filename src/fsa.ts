33300d242154e7c7806efcf8769e3f7c593ef916
Jason Dent <jason@streetsidesoftware.nl>
2023-01-01 23:06:38 +0100

    Update fsa.ts

--- src/fsa.ts
+++ src/fsa.ts
@@ -1,13 +1,11 @@
 import * as fs from 'fs/promises';
 import { FileSystemAdapter, PathLike, BufferEncoding } from './FileSystemAdapter.js';
 import { isURL } from './url_helper.js';
-import { fileURLToPath } from 'node:url';
 
 async function readFile(file: PathLike, encoding: BufferEncoding): Promise<string> {
     if (isURL(file)) {
         if (file.protocol === 'file:') {
-            const filePath = fileURLToPath(file);
-            return await fs.readFile(filePath, encoding);
+            return await fs.readFile(file, encoding);
         }
     }
     return await fs.readFile(file, encoding);
