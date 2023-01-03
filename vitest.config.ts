3c9cf44b69eb76261ce76620aefa8e42d8395093
Jason Dent <jason@streetsidesoftware.nl>
2023-01-03 10:53:44 +0100

    Add vitest config file

---
+++ vitest.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        // reporters: 'verbose',
+        coverage: {
+            // enabled: true,
+            clean: true,
+            all: true,
+            reporter: ['html', 'text', 'lcov', 'json'],
+            exclude: [
+                'content/**',
+                'fixtures/**',
+                'sample*/**',
+                'bin.mjs',
+                '_snapshots_',
+                '.eslint*',
+                'vitest*',
+                '.prettier*',
+            ],
+        },
+        include: ['src/**/*.test.{ts,mts}'],
+        exclude: ['content/**', 'fixtures/**', 'bin.mjs', '_snapshots_'],
+    },
+});
