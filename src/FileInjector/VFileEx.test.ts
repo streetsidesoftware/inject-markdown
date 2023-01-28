11fbf5675db2df847a98a3c4e78c7b43d9c91cee
Jason Dent <jason@streetsidesoftware.nl>
2023-01-28 13:01:36 +0100

    Create VFileEx.test.ts

---
+++ src/FileInjector/VFileEx.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, test } from 'vitest';
+
+import * as VFileEx from './VFileEx.js';
+
+describe('VFileEx', () => {
+    test('VFileEx', () => {
+        expect(VFileEx).toBeDefined();
+    });
+});
