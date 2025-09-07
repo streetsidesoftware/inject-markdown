6e96c222d8150ed54e5a5c871333835558548f49
Jason Dent <jason@streetsidesoftware.nl>
2025-09-07 08:24:51 +0200

    chore: Use eslint defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,11 @@
 
 // import globals from 'globals';
 import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
     js.configs.recommended,
     tseslint.configs.recommended,
     {
